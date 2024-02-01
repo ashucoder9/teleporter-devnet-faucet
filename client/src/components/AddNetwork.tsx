6fd71acb7678e84500e572c1f3156f03277a64e4
Ashutosh Tripathi <ashucoder9@gmail.com>
2024-02-01 17:33:16 +0530

    new configuration

--- client/src/components/AddNetwork.tsx
+++ client/src/components/AddNetwork.tsx
@@ -5,12 +5,7 @@ export default function AddNetwork(props: any) {
         <div className='footer-buttons'>
             <button className="add-network" onClick={() => {addNetwork(props.config)}}>
                 <img alt='metamask' style={{width: "25px", height: "25px", marginRight: "5px"}} src="/memtamask.webp"/>
-                Add Subnet to Metamask
-            </button>
-
-            <button className="add-network" onClick={() => {window.open(`${props.config.EXPLORER}${props.token?.CONTRACTADDRESS ? "/address/" + props.token.CONTRACTADDRESS : ""}`, '_blank')}}>
-                <img alt="block-explorer" style={{width: "25px", height: "25px"}} src="/avaxblack.webp"/>
-                View Block Explorer
+                Add Network to Your Wallet
             </button>
 
             {
